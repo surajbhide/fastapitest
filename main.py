ac0cc0a122d4c6d261e84c4db408197b5cbe0656
Suraj BHIDE <suraj.freelance@gmail.com>
2022-10-20 17:03:50 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": 'hello world!'}
+
+
+@app.get('/items/{item_id}')
+def get_item(item_id: int):
+    return {'item': item_id}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.post('/items/')
+def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({'price_with_tax': price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
